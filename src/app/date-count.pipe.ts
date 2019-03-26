31f8cc13ca3113ac4acaaa736423418d4e1833db
IanSani <iansani259@gmail.com>
2019-03-26 09:15:59 +0300

    Added pipe componrnt for displaying time since quote was entered

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,8 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: any, args?: any): number {
+    let today:Date =new Date();//get current date and AppRoutingModule//
+    let todayWithNOTime:any =new Date(today.getFullYear(),today.getmonth(),today.getDate())
+    var dateDifference=Math.abs(value-todayWithNoTIme)
+    const secondsInADay=86400;
+
+    var dateDifferenceSeconds=dateDifference*0.001;
+
+    if  (dateCounter>= 1 && value > todayWithNoTime){
+      return dateCounter;
+    }else{
+      return 0;
+    }
   }
 
 }
