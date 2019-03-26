06a8b5cb750d8ae2334c3137ea10de396a86599f
IanSani <iansani259@gmail.com>
2019-03-26 04:53:56 +0300

    Added button for upvoting and downvoting

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input,Output,EventEmitter } from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
-  selector: 'app-quote-details',
-  templateUrl: './quote-details.component.html',
-  styleUrls: ['./quote-details.component.css']
+ selector: 'app-quote-details',
+ templateUrl: './quote-details.component.html',
+ styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
 
-  constructor() { }
+@Input() quote: Quote;
+@Output() isComplete= new EventEmitter<boolean>();
 
-  ngOnInit() {
-  }
+quoteDelete(complete:boolean){
+ this.isComplete.emit(complete);
+}
+
+upvotes: number = 0;
+downvote: number = 0;
+thumbsUp() {
+  this.upvotes++;
+}
+thumbsDown() {
+  this.downvote--;
+}
+ constructor() { }
+
+ ngOnInit() {
+ }
 
 }
