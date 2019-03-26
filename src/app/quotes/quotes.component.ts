4b049342ecfa61ad595426452e0aa1505fcc3147
IanSani <iansani259@gmail.com>
2019-03-26 08:23:12 +0300

    removed form repetition

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -11,10 +11,10 @@ export class QuoteComponent implements OnInit {
  quotes = [
    new Quote(1, 'Things change. And friends leave. Life does not stop for anybody', 'Stephen chibosky', new Date(2019, 1, 18)),
    new Quote(2, 'Education is the most powerful weapon which you can use to change the world', 'Nelson Mandela', new Date(2019, 0, 7)),
-   new Quote(3, 'Nurture your mind with great thoughts. To believe in the heroic makes heroes.', 'Benjamin Disrael', new Date(2019, 6, 26)),
-   new Quote(4, 'When I let go of what I am, I become what I might be', 'Jantrix Jane', new Date(2019, 7, 27)),
-   new Quote(5, 'Luck is a dividend of sweat. The more you sweat, the luckier you get.', 'Barbra Gee', new Date(2019, 12, 18)),
-   new Quote(6, 'Those who cannot change their minds cannot change anything', 'George Benard shaw', new Date(2019, 6, 23)),
+   new Quote(3, 'Nurture your mind with great thoughts. To believe in the heroic makes heroes.', 'Benjamin Disrael', new Date(2019, 3, 8)),
+   new Quote(4, 'I do not believe in taking the right decision, I take a decision and make it right', 'Mohamed Ali Jinna', new Date(2019, 7, 14)),
+   new Quote(5, 'Luck is a dividend of sweat. The more you sweat, the luckier you get.', 'Barbra Gee', new Date(2019, 9, 8)),
+   new Quote(6, 'Those who cannot change their minds cannot change anything', 'George Benard shaw', new Date(2019, 11, 13)),
  ]
  deleteQuote(isComplete, index) {
    if (isComplete) {
